e2d81197f5e38a175b6afb51b230ceeb12d764ad
Deepak <deepakck.ptf@gmail.com>
2023-07-12 14:06:18 +0530

    signup , fix

--- lib/authentication/signupPage.dart
+++ lib/authentication/signupPage.dart
@@ -33,135 +33,137 @@ class _SignUpPageState extends State<SignUpPage> {
             child: Column(
               children: [
 
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xffeef4f4),
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(40.0),
-                        // bottomRight: Radius.circular(40.0),
-                        topLeft: Radius.circular(40.0),
-                        // bottomLeft: Radius.circular(40.0)
-                    ),
-                  ),
-
-                  child: Column(
-                    children: [
-
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          height: 100,
-                          width: 100,
-                          color: Colors.red,
-                        ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xffeef4f4),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(40.0),
+                          // bottomRight: Radius.circular(40.0),
+                          topLeft: Radius.circular(40.0),
+                          // bottomLeft: Radius.circular(40.0)
                       ),
+                    ),
 
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20),
-                        child: Container(
-                          // height: MediaQuery.of(context).size.height/1.5,
-                          decoration: BoxDecoration(
-                            color: Color(0xffffffff),
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(40.0),
-                              // bottomRight: Radius.circular(40.0),
-                              topLeft: Radius.circular(40.0),
-                              // bottomLeft: Radius.circular(40.0)
+                    child: Column(
+                      children: [
+
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 100,
+                            width: 100,
+                            color: Colors.greenAccent,
+                          ),
+                        ),
+
+                        Expanded(
+                          child: Container(
+                            // height: MediaQuery.of(context).size.height/1.5,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(40.0),
+                                // bottomRight: Radius.circular(40.0),
+                                topLeft: Radius.circular(40.0),
+                                // bottomLeft: Radius.circular(40.0)
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 16, top: 50),
+                                    child: TextFormField(
+                                      // controller: _usernameController,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(10.0),
+                                        ),
+                                        hintText: 'Name',
+                                        labelText: 'Name',
+                                        contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                                      ),
+                                      textInputAction: TextInputAction.next,
+                                    ),
+                                  ),
+
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 16),
+                                    child: TextFormField(
+                                      // controller: _usernameController,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(10.0),
+                                        ),
+                                        hintText: 'Phone',
+                                        labelText: 'Phone',
+                                        contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                                      ),
+                                      keyboardType: TextInputType.phone,
+                                      inputFormatters: [
+                                        FilteringTextInputFormatter.digitsOnly,
+                                      ],
+                                      textInputAction: TextInputAction.next,
+                                    ),
+                                  ),
+
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 16),
+                                    child: TextFormField(
+                                      // controller: _usernameController,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(10.0),
+                                        ),
+                                        hintText: 'Address',
+                                        labelText: 'Address',
+                                        contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                                      ),
+                                    ),
+                                  ),
+
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 16),
+                                    child: TextFormField(
+                                      // controller: _passwordController,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(10.0),
+                                        ),
+                                        hintText: 'Password',
+                                        labelText: 'Password',
+                                        suffixIcon: IconButton(
+                                            onPressed: (){
+                                              setState(() {
+                                                _obscureText = !_obscureText;
+                                              });
+                                            },
+                                            icon: Icon(
+                                          _obscureText
+                                            ? Icons.visibility_off
+                                            : Icons.visibility,
+                                            ),
+                                        ),
+                                        contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                                      ),
+                                      obscureText: _obscureText,
+                                    ),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {},
+                                    child: Text('SignUp'),
+                                  ),
+                                ],
+                              ),
+
                             ),
                           ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(bottom: 16, top: 50),
-                                child: TextFormField(
-                                  // controller: _usernameController,
-                                  decoration: InputDecoration(
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                    ),
-                                    hintText: 'Name',
-                                    labelText: 'Name',
-                                    contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-                                  ),
-                                  textInputAction: TextInputAction.next,
-                                ),
-                              ),
-
-                              Padding(
-                                padding: const EdgeInsets.only(bottom: 16),
-                                child: TextFormField(
-                                  // controller: _usernameController,
-                                  decoration: InputDecoration(
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                    ),
-                                    hintText: 'Phone',
-                                    labelText: 'Phone',
-                                    contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-                                  ),
-                                  keyboardType: TextInputType.phone,
-                                  inputFormatters: [
-                                    FilteringTextInputFormatter.digitsOnly,
-                                  ],
-                                  textInputAction: TextInputAction.next,
-                                ),
-                              ),
-
-                              Padding(
-                                padding: const EdgeInsets.only(bottom: 16),
-                                child: TextFormField(
-                                  // controller: _usernameController,
-                                  decoration: InputDecoration(
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                    ),
-                                    hintText: 'Address',
-                                    labelText: 'Address',
-                                    contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-                                  ),
-                                ),
-                              ),
-
-                              Padding(
-                                padding: const EdgeInsets.only(bottom: 16),
-                                child: TextFormField(
-                                  // controller: _passwordController,
-                                  decoration: InputDecoration(
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                    ),
-                                    hintText: 'Password',
-                                    labelText: 'Password',
-                                    suffixIcon: IconButton(
-                                        onPressed: (){
-                                          setState(() {
-                                            _obscureText = !_obscureText;
-                                          });
-                                        },
-                                        icon: Icon(
-                                      _obscureText
-                                        ? Icons.visibility_off
-                                        : Icons.visibility,
-                                        ),
-                                    ),
-                                    contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-                                  ),
-                                  obscureText: _obscureText,
-                                ),
-                              ),
-
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Text('SignUp'),
-                              ),
-
-
-                            ],
-                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ],
